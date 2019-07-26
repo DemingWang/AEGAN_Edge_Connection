0c5e6c35ab85fb09e7a3f3babbdb815251383a69
SeanYan <sean627652952@163.com>
2019-07-26 10:13:58 +0800

    update region.py

--- region.py
+++ region.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import glob
+from PIL import Image
 
 def regionGenerate(img):
     findcontourimg = img.copy()
@@ -33,11 +35,24 @@ def regionGenerate(img):
     cv2.drawContours(regionOut, c_max, -1,  (255,255,255), cv2.FILLED)
     if isHoleExist:
         cv2.drawContours(regionOut, c_min, -1,  (0,0,0), cv2.FILLED)
+        cv2.drawContours(regionOut, c_min, -1,  (255,255,255), 1)
     print("mask channel: ",regionOut.shape)
     # cv2.imshow('region',regionOut)
     # cv2.waitKey(0)
-    cv2.imwrite("./Template/bin_mask/bin_mask_{}.png".format("%02d"%i),regionOut)
+    # cv2.imwrite("./Template/bin_mask/bin_mask_{}.png".format("%02d"%i),regionOut)
     return regionOut
     
-
+if __name__ == '__main__':
+    img_dir = ("./Template/bin_contour/")
+    img_files = glob.glob(img_dir + "*.png")
+    img_files.sort(key=lambda x:int(x[-6:-4]))
+    template_imglist = []
+    for tempID,img_file in enumerate(img_files):
+        template_img = Image.open(img_file)
+        template_img = template_img.convert("L")
+        data = np.asarray(template_img)
+        data = regionGenerate(data)
+        sub_img = template_img - data
+        cv2.imshow('sub',sub_img)
+        cv2.waitKey(0)
 
